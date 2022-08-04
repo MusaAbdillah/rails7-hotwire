7c6b8ad8f2f25ae0f6e8f82ef3b082ae0b885682
Musa Abdillah <musa.abdillah@ottodigital.id>
2022-08-04 21:50:45 +0700

    final chapter

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -4,4 +4,8 @@ class QuoteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "#total_price return the total_price of all line items" do
+    assert_equal 2500, quotes(:first).total_price
+  end
 end
